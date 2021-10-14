578badab38cad055c8fd47e75d11b341b1c21a03
charlie1099 <cdcharlesworth@gmail.com>
2021-10-14 00:56:04 -0600

    added updated pw and pw check

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -36,18 +36,27 @@ router.get('/:id', (req, res) => {
 });
 
 //post api users
-router.post('/', (req, res) => {
-    User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+      User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
+        }
+    
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password)
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!' })
+      });  
     });
-});
 
 //put api user 1
 router.put('/:id', (req, res) => {
