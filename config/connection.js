878b2da15ea4105071a8ad5debcb955b1d218f07
charlie1099 <cdcharlesworth@gmail.com>
2021-10-20 21:29:19 -0600

    merge conflict

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,6 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-<<<<<<< HEAD
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
-}
-=======
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -24,7 +11,6 @@ const sequelize = process.env.JAWSDB_URL
       dialect: 'mysql',
       port: 3306
     });
->>>>>>> develop
 
 module.exports = sequelize;
 
